caa8356c626c8ca7c9aa1f8c2346578bad723049
t-keshi <quick.resp.biz094@gmail.com>
2022-06-03 22:04:22 +0900

    nest in backend

---
+++ backend/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,13 @@
+package com.example
+
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSerialization
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
