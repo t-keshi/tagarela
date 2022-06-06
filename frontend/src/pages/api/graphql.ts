b89854c33c10f004cbe2c2fcd3275c67ab466f0d
t-keshi <quick.resp.biz094@gmail.com>
2022-06-06 23:50:20 +0900

    logging

--- frontend/src/pages/api/graphql.ts
+++ frontend/src/pages/api/graphql.ts
@@ -19,11 +19,13 @@ export const config = {
 const startServer = server.start();
 
 const serverLaunch = async (req: MicroRequest, res: ServerResponse) => {
+  console.log(req, res);
   if (req.method === "OPTIONS") {
     res.end();
     return false;
   }
   await startServer;
+  console.log("launched server");
   await server.createHandler({
     path: "/api/graphql",
   })(req, res);
